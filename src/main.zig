d24ae626ca8af61357042bc6be69fab8f17539d9
Jake Stannix <jakestannix@gmail.com>
2024-06-29 11:53:33 -0600

    added game state

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,156 @@ const Rectangle = ray.Rectangle;
 const Color = ray.Color;
 const Vector2 = ray.Vector2;
 
+const BRICK_COUNT: usize = 12;
+const PLAT_SPEED: f32 = 3;
+const BALL_SPEED: f32 = 3;
+
+const screen = struct {
+    pub const width: i32 = 800;
+    pub const height: i32 = 450;
+    pub const fps: i32 = 60;
+};
+const brick_size: f32 = @as(f32, @floatFromInt(screen.width)) / @as(f32, @floatFromInt(BRICK_COUNT));
+const ball_def_pos: Vector2 = .{ @as(f32, @floatFromInt(screen.width / 2)) - 3.5, @as(f32, @floatFromInt(screen.height)) - 60 };
+const plat_def_pos: Rectangle = .{ .x = (screen.width / 2) - 25, .y = screen.height - 20, .height = 10, .width = 40 };
+const def_ball: Ball = .{
+    .pos = ball_def_pos,
+    .dir = .{ 3, 1 },
+    .rad = 7,
+};
+
+pub fn main() !void {
+    const texts = setup(.{
+        .width = screen.width,
+        .height = screen.height,
+        .fps = screen.fps,
+    });
+    defer shutdown(texts);
+    var bricks: [BRICK_COUNT * 3]Brick = undefined;
+    setupBricks(&bricks);
+    var platform: Rectangle = plat_def_pos;
+    var ball: Ball = def_ball;
+    var game_over = false;
+    var victory = false;
+    var frame: f32 = 0;
+    var paused = false;
+    while (!ray.windowShouldClose()) : (frame += if (!game_over) 1 else 0) {
+        if (ray.isKeyPressed(.Space)) {
+            if (game_over or victory) {
+                setupBricks(&bricks);
+                platform = plat_def_pos;
+                ball = def_ball;
+                game_over = false;
+                victory = false;
+            } else {
+                paused = !paused;
+            }
+        }
+        if (!paused and !game_over and !victory) {
+            const delta = ray.getFrameTime();
+            if (ray.isKeyDown(.Right)) {
+                platform.x += (PLAT_SPEED * delta) * screen.fps;
+            }
+            if (ray.isKeyDown(.Left)) {
+                platform.x -= (PLAT_SPEED * delta) * screen.fps;
+            }
+
+            if (@mod(frame, BALL_SPEED) == 0)
+                ball.pos += ball.dir;
+
+            if (ray.checkCollisionLines(
+                .{ platform.x, platform.y },
+                .{ platform.x + platform.width, platform.y },
+                ball.bottom().start,
+                ball.bottom().end,
+            ) != null)
+                ball.dir *= .{ 1, -1 };
+
+            const ball_lu_check = (ball.pos - Vector2{ ball.rad, ball.rad }) < Vector2{ 0, 0 };
+            const ball_rd_check = (ball.pos + Vector2{ ball.rad, ball.rad }) > Vector2{ screen.width, screen.height };
+            if (ball_lu_check[0]) {
+                ball.dir *= Vector2{ -1, 1 };
+            }
+            if (ball_lu_check[1]) {
+                ball.dir *= Vector2{ 1, -1 };
+            }
+            if (ball_rd_check[0]) {
+                ball.dir *= Vector2{ -1, 1 };
+            }
+            if (ball_rd_check[1]) {
+                game_over = true;
+                ball.dir *= Vector2{ 1, -1 };
+            }
+
+            bricks: for (&bricks) |*brick| {
+                if (brick.hit)
+                    continue :bricks;
+                if (ray.checkCollisionCircleRec(ball.pos, ball.rad, brick.rec)) {
+                    brick.hit = true;
+                    var edge = brick.bottom();
+                    var ball_axis = ball.top();
+                    if (ray.checkCollisionLines(edge.start, edge.end, ball_axis.start, ball_axis.end)) |_| {
+                        ball.dir *= Vector2{ 1, -1 };
+                        break :bricks;
+                    }
+                    edge = brick.left();
+                    ball_axis = ball.right();
+                    if (ray.checkCollisionLines(edge.start, edge.end, ball_axis.start, ball_axis.end)) |_| {
+                        ball.dir *= Vector2{ -1, 1 };
+                        break :bricks;
+                    }
+                    edge = brick.right();
+                    ball_axis = ball.left();
+                    if (ray.checkCollisionLines(edge.start, edge.end, ball_axis.start, ball_axis.end)) |_| {
+                        ball.dir *= Vector2{ -1, 1 };
+                        break :bricks;
+                    }
+                    edge = brick.top();
+                    ball_axis = ball.bottom();
+                    if (ray.checkCollisionLines(edge.start, edge.end, ball_axis.start, ball_axis.end)) |_| {
+                        ball.dir *= Vector2{ 1, -1 };
+                        break :bricks;
+                    }
+                }
+            }
+            var check_win = true;
+            for (bricks) |brick|
+                if (!brick.destroied) {
+                    check_win = false;
+                };
+            if (check_win)
+                victory = true;
+            {
+                ray.beginTextureMode(texts.screen.texture);
+                defer ray.endTextureMode();
+                ray.clearBackground(ray.colors.Blank);
+                for (&bricks) |*brick|
+                    if (!brick.hit or !brick.destroied) {
+                        ray.drawTexturePro(texts.brick.texture, texts.brick.source, brick.rec, .{ 0, 0 }, 0, brick.color);
+                        if (brick.hit) {
+                            brick.destroied = true;
+                        }
+                    };
+                ray.drawRectangleRec(platform, ray.colors.Black);
+                ray.drawCircleV(ball.pos, ball.rad, ray.colors.Green);
+            }
+        }
+
+        ray.beginDrawing();
+        defer ray.endDrawing();
+        ray.clearBackground(ray.colors.RayWhite);
+        ray.drawTextureRec(texts.screen.texture.texture, texts.screen.source, .{ 0, 0 }, ray.colors.White);
+        if (game_over) {
+            ray.drawText("GAME OVER", (screen.width / 2) - (5 * 24), (screen.height / 2) - 12, 24, ray.colors.Red);
+        }
+        if (victory) {
+            ray.drawText("VICTORY", (screen.width / 2) - (4 * 24), (screen.height / 2) - 12, 24, ray.colors.Green);
+        }
+        if (paused) {
+            ray.drawText("PAUSED", (screen.width / 2) - (3 * 23), (screen.height / 2) - 12, 24, ray.colors.Black);
+        }
+    }
+}
 const Line = struct {
     start: Vector2,
     end: Vector2,
@@ -95,123 +245,6 @@ const Ball = struct {
         };
     }
 };
-
-const BRICK_COUNT: usize = 12;
-const PLAT_SPEED: f32 = 3;
-const BALL_SPEED: f32 = 3;
-
-pub fn main() !void {
-    const screen = struct {
-        pub const width: i32 = 800;
-        pub const height: i32 = 450;
-        pub const fps: i32 = 60;
-    };
-    const brick_size: f32 = @as(f32, @floatFromInt(screen.width)) / @as(f32, @floatFromInt(BRICK_COUNT));
-    const texts = setup(.{
-        .width = screen.width,
-        .height = screen.height,
-        .fps = screen.fps,
-    });
-    defer shutdown(texts);
-    var bricks: [BRICK_COUNT * 3]Brick = undefined;
-    setupBricks(&bricks, brick_size);
-    const ball_def_pos: Vector2 = .{ @as(f32, @floatFromInt(screen.width / 2)) - 3.5, @as(f32, @floatFromInt(screen.height)) - 60 };
-    const plat_def_pos: Rectangle = .{ .x = (screen.width / 2) - 25, .y = screen.height - 20, .height = 10, .width = 40 };
-    var platform: Rectangle = plat_def_pos;
-    var ball: Ball = .{
-        .pos = ball_def_pos,
-        .dir = .{ 3, 1 },
-        .rad = 7,
-    };
-    var frame: f32 = 0;
-    while (!ray.windowShouldClose()) : (frame += 1) {
-        const delta = ray.getFrameTime();
-        if (ray.isKeyDown(.Right)) {
-            platform.x += (PLAT_SPEED * delta) * screen.fps;
-        }
-        if (ray.isKeyDown(.Left)) {
-            platform.x -= (PLAT_SPEED * delta) * screen.fps;
-        }
-
-        if (@mod(frame, BALL_SPEED) == 0)
-            ball.pos += ball.dir;
-
-        if (ray.checkCollisionLines(
-            .{ platform.x, platform.y },
-            .{ platform.x + platform.width, platform.y },
-            ball.bottom().start,
-            ball.bottom().end,
-        ) != null)
-            ball.dir *= .{ 1, -1 };
-
-        const ball_lu_check = (ball.pos - Vector2{ ball.rad, ball.rad }) < Vector2{ 0, 0 };
-        const ball_rd_check = (ball.pos + Vector2{ ball.rad, ball.rad }) > Vector2{ screen.width, screen.height };
-        if (ball_lu_check[0]) {
-            ball.dir *= Vector2{ -1, 1 };
-        }
-        if (ball_lu_check[1]) {
-            ball.dir *= Vector2{ 1, -1 };
-        }
-        if (ball_rd_check[0]) {
-            ball.dir *= Vector2{ -1, 1 };
-        }
-        if (ball_rd_check[1]) {
-            ball.dir *= Vector2{ 1, -1 };
-        }
-
-        bricks: for (&bricks) |*brick| {
-            if (brick.hit)
-                continue :bricks;
-            if (ray.checkCollisionCircleRec(ball.pos, ball.rad, brick.rec)) {
-                brick.hit = true;
-                var edge = brick.bottom();
-                var ball_axis = ball.top();
-                if (ray.checkCollisionLines(edge.start, edge.end, ball_axis.start, ball_axis.end)) |_| {
-                    ball.dir *= Vector2{ 1, -1 };
-                    break :bricks;
-                }
-                edge = brick.left();
-                ball_axis = ball.right();
-                if (ray.checkCollisionLines(edge.start, edge.end, ball_axis.start, ball_axis.end)) |_| {
-                    ball.dir *= Vector2{ -1, 1 };
-                    break :bricks;
-                }
-                edge = brick.right();
-                ball_axis = ball.left();
-                if (ray.checkCollisionLines(edge.start, edge.end, ball_axis.start, ball_axis.end)) |_| {
-                    ball.dir *= Vector2{ -1, 1 };
-                    break :bricks;
-                }
-                edge = brick.top();
-                ball_axis = ball.bottom();
-                if (ray.checkCollisionLines(edge.start, edge.end, ball_axis.start, ball_axis.end)) |_| {
-                    ball.dir *= Vector2{ 1, -1 };
-                    break :bricks;
-                }
-            }
-        }
-
-        {
-            ray.beginTextureMode(texts.screen.texture);
-            defer ray.endTextureMode();
-            ray.clearBackground(ray.colors.Blank);
-            for (&bricks) |*brick|
-                if (!brick.hit or !brick.destroied) {
-                    ray.drawTexturePro(texts.brick.texture, texts.brick.source, brick.rec, .{ 0, 0 }, 0, brick.color);
-                    if (brick.hit) {
-                        brick.destroied = true;
-                    }
-                };
-            ray.drawRectangleRec(platform, ray.colors.Black);
-            ray.drawCircleV(ball.pos, ball.rad, ray.colors.Green);
-        }
-
-        ray.beginDrawing();
-        defer ray.endDrawing();
-        ray.clearBackground(ray.colors.RayWhite);
-        ray.drawTextureRec(texts.screen.texture.texture, texts.screen.source, .{ 0, 0 }, ray.colors.White);
-    }
-}
 const LoadedTextures = struct {
     brick: struct {
         texture: ray.Texture2D,
@@ -276,7 +309,7 @@ pub const BrickIter = struct {
         return self.buff[self.i * self.row_len .. (self.i + 1) * self.row_len];
     }
 };
-fn setupBricks(bricks: []Brick, brick_size: f32) void {
+fn setupBricks(bricks: []Brick) void {
     var iter: BrickIter = .{
         .buff = bricks,
         .row_len = BRICK_COUNT,
